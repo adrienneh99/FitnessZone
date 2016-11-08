457558070e9c26c8a4dbb55ab82ad7553a74a54c
ahmajor <aggieade@gmail.com>
2016-11-08 10:42:21 -0700

    - Partially finished the FitnessJournalActivity layout

--- app/src/main/java/org/tpl/fitnesszone/LocationActivity.java
+++ app/src/main/java/org/tpl/fitnesszone/LocationActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -32,6 +31,7 @@ public class LocationActivity extends AppCompatActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     /**
