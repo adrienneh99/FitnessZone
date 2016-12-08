2f05176f3044053a42174906a8068394c936d279
ahmajor <aggieade@gmail.com>
2016-12-08 00:49:50 -0700

    - Deleted unused strings (strings.xml) - Finished the Workout Tracker UI (activity_workout_tracker.xml, WorkoutTrackerActivity.java)

--- app/src/main/java/org/tpl/fitnesszone/activity/WorkoutTrackerActivity.java
+++ app/src/main/java/org/tpl/fitnesszone/activity/WorkoutTrackerActivity.java
@@ -5,12 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.Spinner;
+
+import com.google.gson.Gson;
 
 import org.tpl.fitnesszone.R;
 import org.tpl.fitnesszone.fragment.DatePickerFragment;
+import org.tpl.fitnesszone.model.EquipmentExercise;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
 
 public class WorkoutTrackerActivity extends AppCompatActivity {
+
+    private EquipmentExercise[] equipmentExercises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,33 @@ public class WorkoutTrackerActivity extends AppCompatActivity {
         Toolbar fzToolbar = (Toolbar) findViewById(R.id.fz_toolbar);
         fzToolbar.setTitle(R.string.daily_workout_tracker);
         setSupportActionBar(fzToolbar);
+
+        initializeData();
+
+        // Supply the spinner with the array using an instance of ArrayAdapter
+        Spinner spinner1 = (Spinner)findViewById(R.id.exercise_spinner1);
+        Spinner spinner2 = (Spinner)findViewById(R.id.exercise_spinner2);
+        Spinner spinner3 = (Spinner)findViewById(R.id.exercise_spinner3);
+        Spinner spinner4 = (Spinner)findViewById(R.id.exercise_spinner4);
+        Spinner spinner5 = (Spinner)findViewById(R.id.exercise_spinner5);
+
+
+        ArrayList<String> equipmentExerciseNames = new ArrayList<String>();
+        for (int i = 0; i < equipmentExercises.length; i++) {
+            equipmentExerciseNames.add(equipmentExercises[i].getExerciseName());
+        }
+
+        // Creating an ArrayAdapter using the string array and default simple spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.
+                simple_spinner_dropdown_item, equipmentExerciseNames);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+        spinner3.setAdapter(adapter);
+        spinner4.setAdapter(adapter);
+        spinner5.setAdapter(adapter);
+
     }
 
     // Method to show the DatePickerDialog when the user clicks the Date button
@@ -45,6 +85,20 @@ public class WorkoutTrackerActivity extends AppCompatActivity {
             // **POPULATE THE EXERCISE SPINNER WITH ALL CARDIO CIRCUIT EXERCISES**
         }
     }
+
+    private void initializeData() {
+        // Loading the equipmentExercise info from the json file in the raw resource folder
+        Gson gson = new Gson();
+        InputStream inputStream = getResources().openRawResource(R.raw.equipment_exercise);
+        Reader rd = new BufferedReader(new InputStreamReader(inputStream));
+        equipmentExercises = gson.fromJson(rd, EquipmentExercise[].class);
+    }
+
+
+
+
+
+
 
 
 }
